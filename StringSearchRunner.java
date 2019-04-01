8a4690b0627d3b440d39cb8d30dc3a67e58e2d25
Mitchell Wagner <mitchwag17@yahoo.com>
2019-04-01 18:36:32 -0400

    Add files via upload

---
+++ StringSearchRunner.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+import static java.lang.System.*;
+
+public class StringSearchRunner
+{
+	public static void main(String args[])
+	{
+		System.out.println(StringSearch.countLetters("computerscience", "t"));
+		System.out.println(StringSearch.countWords("computerscience", "to"));
+
+		//add more test cases
+	}
+}
