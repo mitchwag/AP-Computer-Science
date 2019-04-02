fb427dc76a096be0264c454f4453a4980b5807f4
Mitchell Wagner <mitchwag17@yahoo.com>
2019-04-01 20:10:20 -0400

    Add files via upload

---
+++ 2018_12_04 - Number of Occurances Lab/RandomOccurancesRunner.java
@@ -0,0 +1,9 @@
+public class RandomOccurancesRunner {
+
+	public static void main(String[] args) {
+		RandomOccurances obj = new RandomOccurances();
+		obj.setNumbers();
+		System.out.println(obj.getCounts());
+	}
+
+}
