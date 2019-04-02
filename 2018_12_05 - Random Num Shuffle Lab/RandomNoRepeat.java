f095ab68ce6c680a7fbe166c044d20e9e7d17787
Mitchell Wagner <mitchwag17@yahoo.com>
2019-04-01 20:14:22 -0400

    Add files via upload

---
+++ 2018_12_05 - Random Num Shuffle Lab/RandomNoRepeat.java
@@ -0,0 +1,32 @@
+
+public class RandomNoRepeat {
+	
+	boolean[] arrUsed = new boolean[100];
+	String nums = "";
+	
+	RandomNoRepeat() {
+		for (int i = 0; i < arrUsed.length; i++)
+			arrUsed[i] = false;
+	}
+	
+	void setNumbers() {
+		int usedCount = 0;
+		do {
+			int temp = (int) (Math.random() * 100) + 1;
+			
+			if (arrUsed[temp - 1] == false) {
+				if ((usedCount + 1) % 10 == 0) {
+					nums += "| " + temp + " |\n";
+				} else {
+					nums += "| " + temp + " ";
+				}
+				arrUsed[temp - 1] = true;
+				usedCount++;
+			}
+		} while (usedCount < 100);
+	}
+	
+	String getNumbers() {
+		return nums;
+	}
+}
