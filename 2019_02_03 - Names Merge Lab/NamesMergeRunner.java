dac6a8850d62c9073f5b620818e7487120cb6572
Mitchell Wagner <mitchwag17@yahoo.com>
2019-04-01 20:28:15 -0400

    Update NamesMergeRunner.java

--- 2019_02_03 - Names Merge Lab/NamesMergeRunner.java
+++ 2019_02_03 - Names Merge Lab/NamesMergeRunner.java
@@ -3,34 +3,34 @@ public class NamesMergeRunner {
 
 	public static void main(String[] args) {
 		String[] arrA = new String[10000];
-        String[] arrB = new String[10000];
-        Scanner scan = new Scanner(System.in);
+        	String[] arrB = new String[10000];
+		Scanner scan = new Scanner(System.in);
 
-        System.out.println("Enter the values for the first array, up to 10000 values, enter 'End' to quit");
-        for (int a = 0; a <= 10000; a++) {
-        	String input = scan.nextLine();
-            if (input.equalsIgnoreCase("End")) {
-                break;
-            } else {
-            	arrA[a] = input;
-            }
-        }
+		System.out.println("Enter the values for the first array, up to 10000 values, enter 'End' to quit");
+		for (int a = 0; a <= 10000; a++) {
+			String input = scan.nextLine();
+		    if (input.equalsIgnoreCase("End")) {
+			break;
+		    } else {
+			arrA[a] = input;
+		    }
+		}
 
-        System.out.println("Enter the values for the second array, up to 10000 values, enter 'End' to quit");
-        for (int b = 0; b <= 10000; b++) {
-        	String input = scan.nextLine();
-            if (input.equalsIgnoreCase("End")) {
-                break;
-            } else {
-                arrB[b] = input;
-            }
-        }
+		System.out.println("Enter the values for the second array, up to 10000 values, enter 'End' to quit");
+		for (int b = 0; b <= 10000; b++) {
+			String input = scan.nextLine();
+		    if (input.equalsIgnoreCase("End")) {
+			break;
+		    } else {
+			arrB[b] = input;
+		    }
+		}
 
-        NamesMerge obj = new NamesMerge(arrA, arrB);
-       
-        System.out.println(obj.getArrA());
-        System.out.println(obj.getArrB());
-        System.out.println(obj.mergeArrays());
+		NamesMerge obj = new NamesMerge(arrA, arrB);
+
+		System.out.println(obj.getArrA());
+		System.out.println(obj.getArrB());
+		System.out.println(obj.mergeArrays());
 	}
 
 }
