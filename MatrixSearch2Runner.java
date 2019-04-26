517f5e425b81b523266b38262f4a62be662ef9a0
Mitchell Wagner <mitchwag17@yahoo.com>
2019-04-25 23:27:20 -0400

    Add files via upload

---
+++ MatrixSearch2Runner.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import java.io.*; 
+
+public class MatrixSearch2Runner
+{
+    public static void main(String[] args) throws Exception
+    {
+    	MatrixSearch2 obj = new MatrixSearch2(20, 15, 50);
+    	System.out.println(obj.toString());
+    }
+}
