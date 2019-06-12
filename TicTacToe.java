fa90c86db3cc7a746a4fc769c38752841e630bb1
Mitchell Wagner <mitchwag17@yahoo.com>
2019-06-12 18:16:47 -0400

    Add files via upload

---
+++ TicTacToe.java
@@ -0,0 +1,24 @@
+import javax.swing.JFrame;
+
+public class TicTacToe extends JFrame
+{
+	private static final int WIDTH = 800;
+	private static final int HEIGHT = 600;
+
+	public TicTacToe()
+	{
+		super("TIC TAC TOE");
+		setSize(WIDTH,HEIGHT);
+
+		getContentPane().add(new GameBoard());
+
+		setVisible(true);
+
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+	public static void main( String args[] )
+	{
+		TicTacToe run = new TicTacToe();
+	}
+}
